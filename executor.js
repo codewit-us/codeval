20ae807490894fa91af47796c11322799361c9da
Aditi More <adit.more2001@gmail.com>
2026-02-27 02:15:07 -0800

    Fix Python dataset working directory issue by running execution inside unique temp directory

--- executor.js
+++ executor.js
@@ -578,7 +578,7 @@ function runProgram(command, args, stdin = '', timeout = 3000, workingDir = null
     let killedByEvaluator = false;
 
     const proc = spawn(shell, wrapperArgs, {
-      cwd: workingDir || process.cwd(),   // ✅ FIXED HERE
+      cwd: workingDir || process.cwd(),   // Set working directory
       detached: true,
       stdio: ['pipe', 'pipe', 'pipe']
     });
